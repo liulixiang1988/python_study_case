99adaa23c5a2e374bb88ea891ff23e2013b03668
liulixiang1988 <liulixiang1988@gmail.com>
2013-12-07 11:26:40 +0800

    添加相关联对象，使用admin.StackInline

--- django/mydjangosite/polls/admin.py
+++ django/mydjangosite/polls/admin.py
@@ -4,12 +4,16 @@ from django.contrib import admin
 from polls.models import Choice, Poll
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question']}),
         ('日期信息', {'fields': ['pub_date'], 'classes': ['collapse']})
     ]
+    inlines = [ChoiceInline]
 
-admin.site.register(Choice)
 admin.site.register(Poll, PollAdmin)
-
