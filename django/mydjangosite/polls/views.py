426fbd41ba318a6ebc877e046489f9984bcac46f
liulixiang1988 <liulixiang1988@gmail.com>
2014-01-27 09:52:33 +0800

    添加视图

--- django/mydjangosite/polls/views.py
+++ django/mydjangosite/polls/views.py
@@ -1,6 +1,25 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from polls.models import Poll
 
 
 def index(request):
-    return HttpResponse('你好，这是投票应用的首页')
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {'latest_poll_list': latest_poll_list})
+    return HttpResponse(template.render(context))
+
+
+def detail(request, poll_id):
+    return HttpResponse('投票%s的细节' % poll_id)
+
+
+def results(request, poll_id):
+    return HttpResponse('投票%s的结果' % poll_id)
+
+
+def vote(request, poll_id):
+    return HttpResponse('对投票%s进行投票' % poll_id)
+
