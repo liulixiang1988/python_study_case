fcd42d6686ec0e62dbd82676b5fd767cf0a600f7
liulixiang1988 <liulixiang1988@gmail.com>
2014-01-27 09:57:43 +0800

    使用render渲染视图

--- django/mydjangosite/polls/views.py
+++ django/mydjangosite/polls/views.py
@@ -1,15 +1,14 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 from polls.models import Poll
 
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {'latest_poll_list': latest_poll_list})
-    return HttpResponse(template.render(context))
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, poll_id):
