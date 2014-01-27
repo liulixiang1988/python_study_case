2baf1f6e93a03c1104b9ff64298f9ee6e07a0cb2
liulixiang1988 <liulixiang1988@gmail.com>
2014-01-27 09:51:36 +0800

    添加视图

--- django/mydjangosite/polls/urls.py
+++ django/mydjangosite/polls/urls.py
@@ -5,5 +5,8 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index')
+                       url(r'^$', views.index, name='index'),
+                       url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
+                       url(r'^(?P<poll_id>\d+)/results/$', views.results, name='result'),
+                       url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote')
 )
