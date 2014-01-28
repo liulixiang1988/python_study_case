ef6bbed0beb18aa66bbcabb1e4b0667987cde5e7
liulixiang1988 <liulixiang1988@gmail.com>
2014-01-28 17:08:54 +0800

    使用Generic View

--- django/mydjangosite/polls/urls.py
+++ django/mydjangosite/polls/urls.py
@@ -6,7 +6,7 @@ from polls import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
-                       url(r'^(?P<poll_id>\d+)/results/$', views.results, name='result'),
+                       url(r'^(?P<pk>\d+)/$', views.IndexView.as_view(), name='detail'),
+                       url(r'^(?P<pk>\d+)/results/$', views.ResultView.as_view(), name='result'),
                        url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote')
 )
--- django/mydjangosite/polls/views.py
+++ django/mydjangosite/polls/views.py
@@ -4,22 +4,25 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from polls.models import Poll, Choice
+from django.views import generic
 
 
-def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    context = {'latest_poll_list': latest_poll_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        return Poll.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html', {'poll': poll})
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
 
 
-def results(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/result.html', {'poll': p})
+class ResultView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/result.html'
 
 
 def vote(request, poll_id):
