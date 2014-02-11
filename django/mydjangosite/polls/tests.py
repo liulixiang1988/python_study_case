645edfceef41823415a13ee5246e8bd5a490e927
liulixiang1988 <liulixiang1988@gmail.com>
2014-02-11 22:55:12 +0800

    添加测试

--- django/mydjangosite/polls/tests.py
+++ django/mydjangosite/polls/tests.py
@@ -6,11 +6,16 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.utils import timezone
+import datetime
+
+from polls.models import Poll
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PollMethodTests(TestCase):
+    def test_was_published_recently_with_future_poll(self):
+        '''
+        was_published_recently should return false with future poll
+        '''
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=10))
+        self.assertEqual(future_poll.was_published_recently(), False)
