0bda2dbc63f72f1f37b9aa6ecd72cd143ea39c49
Liu Lixiang <liulixiang1988@gmail.com>
2014-02-23 23:24:02 +0800

    修改settings中的db

--- django/mydjangosite/mydjangosite/settings.py
+++ django/mydjangosite/mydjangosite/settings.py
@@ -14,8 +14,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'djangotutorial', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': 'liulx123',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432', # Set to empty string for default.
     }
