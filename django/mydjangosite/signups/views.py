d41b492da4e6c56be6b946738e004dbc244cd81d
liulixiang1988 <liulixiang1988@gmail.com>
2014-03-22 17:22:45 +0800

    pep8

--- django/mydjangosite/signups/views.py
+++ django/mydjangosite/signups/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, render_to_response, RequestContext
 from .forms import SignUpForm
 from django.contrib import messages
 
+
 # Create your views here.
 def home(request):
     form = SignUpForm(request.POST or None)
