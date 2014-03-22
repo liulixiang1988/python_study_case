50c59b3a14ab3367dce78cb1e950a3318536a3d1
liulixiang1988 <liulixiang1988@gmail.com>
2014-03-22 17:22:16 +0800

    django消息框架的使用，bootstrap中alert alert-success的使用

--- django/mydjangosite/signups/views.py
+++ django/mydjangosite/signups/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, render_to_response, RequestContext
 from .forms import SignUpForm
-
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -8,6 +8,7 @@ def home(request):
     if form.is_valid():
         save_it = form.save(commit=False)
         save_it.save()
+        messages.success(request, "感谢您的加入！")
     return render_to_response('signups/signup.html',
                               locals(),
                               context_instance=RequestContext(request))
