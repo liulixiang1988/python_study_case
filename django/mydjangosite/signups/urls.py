56eb0a41a8d08014a23cb75cf1e3d2ff1e082155
liulixiang1988 <liulixiang1988@gmail.com>
2014-03-23 17:47:27 +0800

    bootstrap template

--- django/mydjangosite/signups/urls.py
+++ django/mydjangosite/signups/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.home, name='signup'),
+                       url(r'^thank-you', views.thank_you, name='thank-you'),
                        )
--- django/mydjangosite/signups/views.py
+++ django/mydjangosite/signups/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, render_to_response, RequestContext
+from django.shortcuts import render, render_to_response, RequestContext, redirect
+from django.core.urlresolvers import reverse
 from .forms import SignUpForm
 from django.contrib import messages
 
@@ -10,7 +11,13 @@ def home(request):
         save_it = form.save(commit=False)
         save_it.save()
         messages.success(request, "感谢您的加入！")
+        return redirect(reverse('signups:thank-you'))
+
     return render_to_response('signups/signup.html',
                               locals(),
                               context_instance=RequestContext(request))
     #return render(request, 'signups/signup.html')
+
+
+def thank_you(request):
+    return render(request, 'signups/thankyou.html')
