065f930e40bf334e016592b4394060ed3b59eae5
liulixiang1988 <liulixiang1988@gmail.com>
2014-04-12 10:34:39 +0800

    rango lesson4 Templates and Static Files

--- django/mydjangosite/rango/views.py
+++ django/mydjangosite/rango/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello, Rango, <a href="/rango/about/">关于</a>')
+    context = RequestContext(request)
+    context_dict = {'boldmessage': '消息在这儿'}
+    return render_to_response('rango/index.html', context_dict, context)
 
 
 def about(request):
-    return HttpResponse('这是关于页面')
+    return render(request, 'rango/about.html')
