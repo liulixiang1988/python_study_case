2302d6cee58987b0b15cec4cca70abac9624aacf
Liu Lixiang <liulixiang1988@gmail.com>
2014-04-17 23:28:06 +0800

    rango - class 6

--- django/mydjangosite/rango/admin.py
+++ django/mydjangosite/rango/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Category, Page
 # Register your models here.
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
