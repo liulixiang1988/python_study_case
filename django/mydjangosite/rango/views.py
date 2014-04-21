7e802df2c4f456b9d2a2afacaa85068411f3becb
liulixiang1988 <liulixiang1988@gmail.com>
2014-04-21 15:40:18 +0800

    rango 6 - create category list

--- django/mydjangosite/rango/views.py
+++ django/mydjangosite/rango/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
 
+from .models import Category
+
 
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': '消息在这儿'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
     return render_to_response('rango/index.html', context_dict, context)
 
 
