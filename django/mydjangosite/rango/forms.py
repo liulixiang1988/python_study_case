806cbd584235cf9df591691ece0c60f89b022399
Liu Lixiang <liulixiang1988@gmail.com>
2014-04-25 07:45:07 +0800

    rango - class 7 forms.py

--- django/mydjangosite/rango/forms.py
+++ django/mydjangosite/rango/forms.py
@@ -1,3 +1,26 @@
 # -*- coding:utf-8 -*-
 __author__ = 'liulixiang'
 
+from django import forms
+from .models import Category, Page
+
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="请输入类型名称")
+    views = forms.IntegerField(widget=forms.HiddenInput, initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput, initial=0)
+
+    class Meta:
+        model = Category
+
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="请输入标题")
+    url = forms.URLField(max_length=128, help_text="请输入网页地址")
+    views = forms.IntegerField(widget=forms.HiddenInput, initial=0)
+
+    class Meta:
+        model = Page
+
+
+
