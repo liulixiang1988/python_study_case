b37183abcec87c6bf2ad6440db78f761b9d00484
liulixiang1988 <liulixiang1988@gmail.com>
2014-04-25 16:48:54 +0800

    rango 8 - authentication add userprofile template

--- django/mydjangosite/rango/urls.py
+++ django/mydjangosite/rango/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
                        url(r'^about/$', views.about, name='about'),
                        url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),
                        url(r'^add_category/$', views.add_category, name='add_category'),
-                       url(r'^category/(?P<category_name_url>\w+)/add_page/$', views.add_page, name='add_page'),)
+                       url(r'^category/(?P<category_name_url>\w+)/add_page/$', views.add_page, name='add_page'),
+                       url(r'^register/$', views.register, name='register'),)
