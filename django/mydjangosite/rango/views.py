208b146c0d1ed556cab72dc0c685ba80fa523fc8
liulixiang1988 <liulixiang1988@gmail.com>
2014-04-25 16:30:06 +0800

    rango 8 - authentication add userprofile view

--- django/mydjangosite/rango/views.py
+++ django/mydjangosite/rango/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, render_to_response, redirect
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
-from .models import Category, Page
-from .forms import CategoryForm, PageForm
+from .models import Category, Page, UserProfile
+from .forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 
 def decode_url(category_name_url):
@@ -78,3 +79,30 @@ def add_page(request, category_name_url):
         form = PageForm()
     return render(request, 'rango/add_page.html', {'form': form,
                                                    'category_name_url': category_name_url})
+
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        userprofile_form = UserProfileForm()
+    return render(request, 'rango/register.html', locals())
