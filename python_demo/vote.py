7faf139d8385a4aed3251c302732ab917c970a24
liulixiang1988 <liulixiang1988@gmail.com>
2014-05-28 16:17:48 +0800

    update vote.py

--- python_demo/vote.py
+++ python_demo/vote.py
@@ -2,6 +2,8 @@
 # -*- coding:utf-8 -*-
 
 import requests
+import requests.exceptions
+from faker import Factory
 import json
 
 from multiprocessing import Pool
@@ -9,16 +11,25 @@ from multiprocessing.dummy import Pool as ThreadPool
 
 url = 'http://365jia.cn/zt/qnwmh/doVote'
 vote_data = {'id': '678998'}
-
+faker = Factory.create(locale='zh_CN')
 
 def vote(proxy_addr):
     proxy = {'http': proxy_addr}
+    ip = faker.ipv4()
+    headers = {'X-Forwarded-For': ip, 'CLIENT_IP': ip}
     try:
         print('start:', proxy_addr)
-        r = requests.post(url, data=vote_data, proxies=proxy)
-        print(proxyaddr, '\t', json.loads(r.text)['message'])
-    except Exception:
-        pass
+        r = requests.post(url, data=vote_data, proxies=proxy, headers=headers)
+        if r.status_code == requests.codes.ok:
+            print(proxy_addr, '\t', r.text)
+        else:
+            print(proxy_addr, '该地址已经被禁用', r.text)
+    except TimeoutError as e:
+        print(e)
+    except requests.exceptions.ProxyError as e:
+        print(e)
+    except requests.exceptions.ConnectionError as e:
+        print(e)
 
 with open('proxy.txt', 'r+', encoding='utf-8') as f:
     proxyaddrs = ['http://' + line.split('@', 1)[0] for line in f.readlines()]
