b90b93b96cfb7a4eade3beffe084b2da17b1db43
liulixiang1988 <liulixiang1988@gmail.com>
2014-06-09 17:01:52 +0800

    vote update

--- python_demo/vote.py
+++ python_demo/vote.py
@@ -1,39 +1,45 @@
-#!/user/bin/env python
+# !/user/bin/env python
 # -*- coding:utf-8 -*-
 
 import requests
 import requests.exceptions
 from faker import Factory
 import json
-
-from multiprocessing import Pool
-from multiprocessing.dummy import Pool as ThreadPool 
+from multiprocessing.dummy import Pool as ThreadPool
 
 url = 'http://365jia.cn/zt/qnwmh/doVote'
 vote_data = {'id': '678998'}
 faker = Factory.create(locale='zh_CN')
+print(url)
+
 
 def vote(proxy_addr):
     proxy = {'http': proxy_addr}
     ip = faker.ipv4()
-    headers = {'X-Forwarded-For': ip, 'CLIENT_IP': ip}
+    headers = {'X-Forwarded-For': ip, 'CLIENT_IP': ip,
+               'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36'}
     try:
-        print('start:', proxy_addr)
         r = requests.post(url, data=vote_data, proxies=proxy, headers=headers)
         if r.status_code == requests.codes.ok:
-            print(proxy_addr, '\t', r.text)
+            print(proxy_addr, '\t', json.loads(r.text)['message'])
         else:
-            print(proxy_addr, '该地址已经被禁用', r.text)
-    except TimeoutError as e:
-        print(e)
-    except requests.exceptions.ProxyError as e:
-        print(e)
-    except requests.exceptions.ConnectionError as e:
-        print(e)
+            print(proxy_addr, 'Error Message:', r.text)
+    except Exception as e:
+        pass
 
-with open('proxy.txt', 'r+', encoding='utf-8') as f:
-    proxyaddrs = ['http://' + line.split('@', 1)[0] for line in f.readlines()]
-    pool = ThreadPool(8)
-    pool.map(vote, proxyaddrs)
-    pool.close()
-    pool.join()
+
+def main():
+    while True:
+        with open('proxy.txt', 'r+', encoding='utf-8') as f:
+            proxyaddrs = ['http://' + line.split('@', 1)[0] for line in f.readlines()]
+            try:
+                pool = ThreadPool(8)
+                pool.map(vote, proxyaddrs)
+                pool.close()
+                pool.join()
+            except Exception as e:
+                print(e)
+
+
+if __name__ == '__main__':
+    main()
