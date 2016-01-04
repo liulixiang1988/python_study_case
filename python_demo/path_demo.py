2d8ddb613811a6d673887f5db18e0bbe60d95177
liulixiang1988 <liulixiang1988@gmail.com>
2016-01-04 12:43:03 +0800

    update path_demo

--- python_demo/path_demo.py
+++ python_demo/path_demo.py
@@ -7,9 +7,10 @@ import sys
 def main():
 	print(__file__)
 	print(os.path.dirname(__file__))
+	path = os.path.abspath(__file__)
 	print(os.path.dirname(os.path.dirname(__file__)))
 	print(sys.path)
-	sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+	sys.path.append(os.path.dirname(os.path.dirname(path)))
 	print(sys.path)
 	print(os.path.dirname('..'))
 
